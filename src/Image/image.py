7e712084b8f5e6a55b6aa1a13112c3290212a203
xiaobiky123 <hujie119121@163.com>
2019-12-13 11:19:56 +0800

    add

---
+++ src/Image/image.py
@@ -0,0 +1,7 @@
+bgLearn  learn the backgroud by pics from cam then get a background model
+
+vector<Vec4d> convertRoiCoordinate(vector<Vec4d> Lines,Rect Roi)
+
+void SamplesEnhance::findrColor(Mat &imgO,Rect &rectout)
+
+checkImage   check the cnn detected result by image process and image track and then update the bottle dict
