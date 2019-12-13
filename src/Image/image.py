543bf5d393d405ce807f87a3e3cdb5ec7ff32e1d
xiaobiky123 <hujie119121@163.com>
2019-12-13 11:38:31 +0800

    add

--- src/Image/image.py
+++ src/Image/image.py
@@ -4,4 +4,13 @@ vector<Vec4d> convertRoiCoordinate(vector<Vec4d> Lines,Rect Roi)
 
 void SamplesEnhance::findrColor(Mat &imgO,Rect &rectout)
 
+
+cvtColor(frame, gray, CV_RGB2GRAY);
+
+
+
+Canny(theobj, theobj, 10, 50);
+
+HoughLinesP(theobj, Lines, 1, CV_PI / 360, 60, 30, 5);
+
 checkImage   check the cnn detected result by image process and image track and then update the bottle dict
