2dfb989a3a365e8f900958eddb145534ab217206
demoivrelu <391771331@qq.com>
2019-12-21 10:15:11 +0800

    settled into main() in main_test_Duplication

--- src/Main/main_test_Duplication.py
+++ src/Main/main_test_Duplication.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.abspath('../../'))
 from lib.Logger.Logger import *
 import time
 import logging
+from src.Image import image
 
 #logger = logging.getLogger(__name__)
 #logger.addHandler(logging.FileHandler("log.txt"))  # 添一个FileHandler
@@ -13,32 +14,31 @@ import logging
 #                    datefmt="%d-%M-%Y %H:%M:%S", level=logging.INFO)    #设置
 
 def main():
-    global gState
-    global gDir
+    gState = image.gState
+    print("gState={},系统初始化···".format(gState))
+    try:
+        cam, _image = image.imageInit()
+    except Exception as e:
+        print("系统初始化失败！--{}".format(e))
+        sys.exit()
+    else:
+        print("系统初始化完成！")
+        print("gState={}".format(image.gState))
 
-    if gState == 1:
-        print("pass...")
-        time.sleep(1)
-        #logger.info("info")
-        #logger.error("error")
-        #logger.addHandler(log)
-        gState = 2
-    elif gState == 2:
-        print("pass...")
-        time.sleep(1)
-        #logger.error("error2")
-        gState = 3
-        #logger.addHandler(log)
-    elif gState == 3:
-        print("gState = 3")
-        time.sleep(1)
-        print("this is debug information ...")
-        gState = 1
+    if image.gState == 2:
+        print("gState={},系统启动中···".format(image.gState))
+        print("开始获取数据···")
+        try:
+            image.imageRun(cam, _image)
+        except Exception as e:
+            print("系统启动失败！--{}".format(e))
+            sys.exit()
+
 
 if __name__ == "__main__":
     sys.stdout = Logger("D:\\12.txt")  # 保存到D盘
-    print("please type the gState: ")
-    gState = int(input())
+    #print("please type the gState: ")
+    #gState = int(input())
     #print("please type the message (debug_test: 1 for bug ; 0 for normal): ")
     #message = int(input())
     # n = 0
