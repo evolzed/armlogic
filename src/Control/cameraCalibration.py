d9a58ecdbfae4172a71027a6724c4d9a927b1243
LYF <502550265@qq.com>
2019-12-24 21:06:05 +0800

    修改了计算过程

--- src/Control/cameraCalibration.py
+++ src/Control/cameraCalibration.py
@@ -11,14 +11,18 @@ import numpy as np
 Zc = 570.2425
 u = 966.0
 v = 777.0
-intrinsicMatrix = np.matrix([[1100.3497, -0.2253, 595.6410, 0.0], [0.0, 1100.3214, 544.8440, 0.0], [0.0, 0.0, 1.0, 0.0]])
-extrinsicMatrix = np.matrix([[0.9994, 0.0315, -0.0176, -61.0168], [-0.0295, 0.9943, 0.1027, -93.4316], [0.0207, -0.1021, 0.9946, 523.6375], [0.0, 0.0, 0.0, 1.0]])
-transferMatrix = np.dot(intrinsicMatrix, extrinsicMatrix)
-coefficientMatrix = transferMatrix[0:3, 0:3]
-equaValue = np.matrix([Zc*u-transferMatrix[0,4]],[Zc*v-transferMatrix[1,4]],[Zc-transferMatrix[2,4]])
-xWorld = coefficientMatrix.I*equaValue
+intrinsicMtx = np.array([[1100.3497, -0.2253, 595.6410, 0.0], [0.0, 1100.3214, 544.8440, 0.0], [0.0, 0.0, 1.0, 0.0]])
+extrinsicMtx = np.array([[0.9994, 0.0315, -0.0176, -61.0168], [-0.0295, 0.9943, 0.1027, -93.4316], [0.0207, -0.1021, 0.9946, 523.6375], [0.0, 0.0, 0.0, 1.0]])
+transferMtx = np.dot(intrinsicMtx, extrinsicMtx)
+coefficientMtx = transferMtx[0:3, 0:3]
+resultMtx = -transferMtx[0:3, 3] + np.array([[u, v, 1]])*Zc #行向量
+resultMtx = resultMtx.reshape(3, 1) #转换成列向量
+Xw = np.dot(np.linalg.inv(coefficientMtx), resultMtx)
+print("Xw =\n",Xw)
 
-print(xWorld)
+
+
+
 
 
 
