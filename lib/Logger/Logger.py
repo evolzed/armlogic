8f3bc25884f457d48cec29852d2c026299addbfa
demoivrelu <391771331@qq.com>
2019-12-24 10:13:10 +0800

    logger update , keyword : bottle timeCost

--- lib/Logger/Logger.py
+++ lib/Logger/Logger.py
@@ -21,7 +21,7 @@ class Logger(object):
 
         msg = currentdatetime + message
         self.terminal.write(msg)
-        if (message.find("box") and (message.find("timeCost") or message.find("bgTimeCost") or message.find("nFrame")) != -1):  #假如message内包含字符串"box"，则写入log，否则不记录
+        if (message.find("bottle") and (message.find("timeCost")) != -1):  #假如message内包含字符串"bottle"以及"timeCost"，则写入log，否则不记录
             self.log.write(msg)
 
 
