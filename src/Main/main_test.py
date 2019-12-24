8b37fcceca93dea6bf21c45020fb799033db5acc
demoivrelu <391771331@qq.com>
2019-12-24 10:36:15 +0800

    commit

--- src/Main/main_test.py
+++ src/Main/main_test.py
@@ -8,10 +8,10 @@ from src.Image import image
 
 
 def main():
-    gState = image.gState
+    gState = image.gState   #gState 从Image获取
     print("gState={},系统初始化···".format(gState))
     try:
-        cam, _image = image.imageInit()
+        cam, _image = image.imageInit()     #相机初始化
     except Exception as e:
         print("系统初始化失败！--{}".format(e))
         sys.exit()
@@ -23,7 +23,7 @@ def main():
         print("gState={},系统启动中···".format(image.gState))
         print("开始获取数据···")
         try:
-            image.imageRun(cam, _image)
+            image.imageRun(cam, _image)     #相机运行
         except Exception as e:
             print("系统启动失败！--{}".format(e))
             sys.exit()
