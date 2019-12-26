51f72078928d17064ec995719d7d31044153ce35
git <git@git.server>
2019-12-26 06:15:49 +0000

    Revert "Merge branch 'MovePlanning' into 'BottleSort0.1'"
    This reverts merge request !41

--- src/Control/Px2W.py
+++ src/Control/Px2W.py
@@ -33,15 +33,17 @@ ExtrinsicMtx = np.array([[0.998164450924708, 0.057618607890134, -0.0186500652808
                          [0.0, 0.0, 0.0, 1.0]])
 
 Zc = 1428.595  # 比例因子
-if __name__ == '__main__':
-    U1 = 532  # 待求的像素坐标
-    V1 = 364  # 待求的像素坐标
-    U2 = 476  # 待求的像素坐标
-    V2 = 522  # 待求的像素坐标
-    PositionMtx1 = Px2World(U1, V1, Zc, IntrinsicMtx, ExtrinsicMtx)  # 调用举例
-    PositionMtx2 = Px2World(U2, V2, Zc, IntrinsicMtx, ExtrinsicMtx)  # 调用举例
-    Length = np.sqrt((PositionMtx2[0]-PositionMtx1[0])**2+(PositionMtx2[1]-PositionMtx1[1])**2)
-    print("Length =\n", Length)
+U1 = 532  # 待求的像素坐标
+V1 = 364  # 待求的像素坐标
+
+U2 = 476  # 待求的像素坐标
+V2 = 522  # 待求的像素坐标
+
+PositionMtx1 = Px2World(U1, V1, Zc, IntrinsicMtx, ExtrinsicMtx)  # 调用举例
+PositionMtx2 = Px2World(U2, V2, Zc, IntrinsicMtx, ExtrinsicMtx)  # 调用举例
+Length = np.sqrt((PositionMtx2[0]-PositionMtx1[0])**2+(PositionMtx2[1]-PositionMtx1[1])**2)
+print("Length =\n", Length)
+
 
 
 
