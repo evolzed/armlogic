e63fbe47a0b392cf99f9deac16d6bbcc294bee19
demoivrelu <391771331@qq.com>
2019-12-30 11:28:42 +0800

    Track update

--- src/Track/Track.py
+++ src/Track/Track.py
@@ -11,16 +11,16 @@ class Track:
         #创建新的target并标记uuid
         trackDict = dict()
         targetList = list()
-        trackFlag = 0
-        position = 0
-        speed = 0
-        angle = 0
-        type = 0
-        typeCounter = 0
+        trackFlag = "0"
+        position = "0"
+        speed = "0"
+        angle = "0"
+        type = "0"
+        typeCounter = "0"
 
         trackDict.setdefault("target", targetList)
 
-        uuID = uuid.uuid1()
+        uuID = str(uuid.uuid1())
         targetList.append(uuID)
         targetList.append(trackFlag)
         targetList.append(position)
@@ -28,18 +28,28 @@ class Track:
         targetList.append(angle)
         targetList.append(type)
         targetList.append(typeCounter)
+        #targetList.append('\n')
 
+        file = open("trackDict_test.txt", "a")
+        for target in targetList:
+            file.writelines(target + ", ")
+        file.writelines("\n")
         print(trackDict)
         return trackDict
 
     def updateTarget(self,bottleDict):
         #将bottleDict中数据进行换算，并更新至trackDict内相对应的target
-        bottleDict = self.bottleDict
-        file = open("trackDict.txt", "r")
+        #bottleDict = self.bottleDict
+        file = open("trackDict_test.txt", "a")
+
         trackDict = json.load(file.read())
+        print(trackDict)
         return trackDict
 
-    #def insertTarget(self):
 
 if __name__ == "__main__":
-    track = Track().createTarget()
+    #os.mknod("trackDict.txt")
+
+    bottledict1 = {'target': ["2add89f6-2aa8-11ea-921f-985fd3d62bfb", "11", "11", "11", "11", "11", "11"]}
+    track1 = Track().createTarget()
+    #track2 = Track().updateTarget(bottledict1)
