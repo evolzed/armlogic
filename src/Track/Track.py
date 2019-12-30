4af0a7c7a7501192249d40e2921667d5ef8da52e
demoivrelu <391771331@qq.com>
2019-12-30 10:00:20 +0800

    update Track

--- src/Track/Track.py
+++ src/Track/Track.py
@@ -11,10 +11,35 @@ class Track:
         #创建新的target并标记uuid
         trackDict = dict()
         targetList = list()
+        trackFlag = 0
+        position = 0
+        speed = 0
+        angle = 0
+        type = 0
+        typeCounter = 0
+
+        trackDict.setdefault("target", targetList)
+
+        uuID = uuid.uuid1()
+        targetList.append(uuID)
+        targetList.append(trackFlag)
+        targetList.append(position)
+        targetList.append(speed)
+        targetList.append(angle)
+        targetList.append(type)
+        targetList.append(typeCounter)
+
+        print(trackDict)
         return trackDict
 
     def updateTarget(self,bottleDict):
         #将bottleDict中数据进行换算，并更新至trackDict内相对应的target
+        bottleDict = self.bottleDict
         file = open("trackDict.txt", "r")
         trackDict = json.load(file.read())
         return trackDict
+
+    #def insertTarget(self):
+
+if __name__ == "__main__":
+    track = Track().createTarget()
