ea27995b89b9786d9e1645b1ba4942a884922dc9
demoivrelu <391771331@qq.com>
2019-12-31 14:10:28 +0800

    commit format

--- src/Track/Track.py
+++ src/Track/Track.py
@@ -2,14 +2,14 @@
 # !/bin/python
 import os
 import sys
-import json
 import uuid
 
-#提供增加新的Target目标功能
-#提供更新实时Target目标功能
 
 class Track:
-
+    """
+    提供增加新的Target目标功能
+    提供更新实时Target目标功能
+    """
     def createTarget(self):
         """
         增加新的Target目标功能
@@ -17,7 +17,7 @@ class Track:
         :return: 新的带UUID的trackDict
         """
 
-        #创建新的target并标记uuid
+        # 创建新的target并标记uuid
         trackDict = dict()
         targetLists = list()
         trackFlag = "0"
@@ -29,7 +29,7 @@ class Track:
 
         trackDict.setdefault("target", targetLists)
 
-        uuID = str(uuid.uuid1())    #自己创建，后期使用LKTrack打上的UUID
+        uuID = str(uuid.uuid1())    # 自己创建，后期使用LKTrack打上的UUID
         targetLists.append(uuID)
         targetLists.append(trackFlag)
         targetLists.append(position)
@@ -37,7 +37,7 @@ class Track:
         targetLists.append(angle)
         targetLists.append(type)
         targetLists.append(typeCounter)
-        #targetLists.append('\n')
+        # targetLists.append('\n')
 
         file = open("trackDict_test.txt", "a")
         for target in targetLists:
@@ -54,7 +54,7 @@ class Track:
         :return: 同一UUID的信息更新；
         """
 
-        #将bottleDict中数据进行换算，并更新至trackDict内相对应的target
+        # 将bottleDict中数据进行换算，并更新至trackDict内相对应的target
 
         file = open("trackDict_test.txt", "r+")
 
@@ -66,11 +66,11 @@ class Track:
                 break
             for targetList in targetLists:
                 # 对比UUID ，假如一样则执行更新
-                #临时tempLists
+                # 临时tempLists
                 tempLists = bottleDict.get("target")
                 print(tempLists)
                 tempSingleList = targetList.split(", ")
-                #print(tempLists)
+                # print(tempLists)
                 if (tempLists[0] in targetList):
                     # 赋值：给与每一个tempSingleList
                     file.write(tempLists[0] + ", ")
@@ -82,14 +82,14 @@ class Track:
 
                     file.write("\n")
                 break
-                #print(targetList)
+                # print(targetList)
         file.close()
 
 
 if __name__ == "__main__":
 
-    #测试用例，此处bottleDict使用的非BS0.1中bottledict，而是将来为Main中提供的传参！
+    # 测试用例，此处bottleDict使用的非BS0.1中bottledict，而是将来为Main中提供的传参！
     bottledict1 = {'target': ["da5b6600-2b6e-11ea-8937-985fd3d62bfb", "11", "66", "11", "11", "11", "11"]}
     bottledict2 = {'target': ["f025d3fe-2b6e-11ea-a086-985fd3d62bfb", "11", "77", "11", "11", "11", "11"]}
-    #track1 = Track().createTarget()        #测试createTarget
-    track2 = Track().updateTarget(bottledict2)      #测试updateTarget
+    # track1 = Track().createTarget()        # 测试createTarget
+    track2 = Track().updateTarget(bottledict2)      # 测试updateTarget
