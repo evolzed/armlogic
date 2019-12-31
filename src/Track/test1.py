0279b0f1245668e1e12486de0203b86f9ecede16
demoivrelu <391771331@qq.com>
2019-12-31 17:32:53 +0800

    test1 update

--- src/Track/test1.py
+++ src/Track/test1.py
@@ -1,8 +1,7 @@
 from src.Vision.camera import Camera
 # from src.Vision.vision import Image
 import cv2
-from src.Vision.imageProcess.imageTrack import ImageTrack
-from src.Vision.imageProcess.bgLearn import Bglearn
+from src.Vision.imageProcess.imgProc import ImgProc
 
 
 if __name__ == '__main__':
@@ -10,18 +9,17 @@ if __name__ == '__main__':
     while True:
         _frame, nFrame, t = cam.getImage()
         cv2.imshow("test", _frame)
-
-        frame, bgMask = Bglearn(10).delBg(_frame) if Bglearn(10) else (_frame, None)
+        tempImgproc = ImgProc(10)
+        frame, bgMask, resarray = tempImgproc.delBg(_frame) if tempImgproc else (_frame, None)
         dataDict = dict()
         #dataDict = ImageTrack.yolo.detectImage(img)
         #bgLearn = Bglearn()
         dataDict["nFrame"] = nFrame
         dataDict["frameTime"] = t  # 相机当前获取打当前帧nFrame的时间t
-        dataDict["bgTimeCost"] = Bglearn(10).bgTimeCost if Bglearn(10) else 0
+        dataDict["bgTimeCost"] = tempImgproc.bgTimeCost if tempImgproc else 0
         print(dataDict)
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
-
 
     cam.destroy()
