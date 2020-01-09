fb6878ceb3b1f64281ddd61eada5b6607f219e28
demoivrelu <391771331@qq.com>
2020-01-09 09:17:50 +0800

    stash test

--- src/Track/stashTest.py
+++ src/Track/stashTest.py
@@ -1,1 +1,11 @@
+<<<<<<< Updated upstream
 # this is stash test
+=======
+# this is stash test
+
+# if you see this line ,stashed ,stash success!
+
+# if you see this line ,stash success again!
+
+# if you see this line ,stash success again!
+>>>>>>> Stashed changes
