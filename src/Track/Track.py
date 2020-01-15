0dab2166e7021e00099a709603049944e2bd1bb6
demoivre <391771331@qq.com>
2020-01-15 17:54:10 +0800

    track modified, Track.speedEstimate()

--- src/Track/Track.py
+++ src/Track/Track.py
@@ -24,7 +24,7 @@ class Track:
         """
         增加新的Target目标功能
 
-        :return: 新的带UUID的targetDict
+        :return: 新的带UUID的targetDict, 传至imgProc的UUID
         """
 
         # 创建新的target并标记uuid 返回给bottleDict
@@ -44,6 +44,8 @@ class Track:
             trackFlag = 0
             position = [int((bottleDict["box"][i][2] + bottleDict["box"][i][4]) / 2),
                         int((bottleDict["box"][i][3] + bottleDict["box"][i][5]) / 2)]
+
+            # 第一帧用传输带速度或估计值，后续用speedEstimate()!!
             speed = [5, 5]
             angle = 0
             type = 0
@@ -82,9 +84,8 @@ class Track:
         :return: 同一UUID下的目标物的信息更新；
         """
 
+        self._nFrame = _nFrame
         deltaT = 0.01
-        # self._frame = _frame
-        # self._frameTime = _frameTime
         oldTargetDict = targetDict
         newTargetDict = oldTargetDict
         startTime = _currentTime
@@ -96,15 +97,13 @@ class Track:
             newTargetDictLists[i][2][1] = newTargetDictLists[i][2][1] + float(newTargetDictLists[i][3][1]) * (deltaT)
             cv2.rectangle(_frame, (int(newTargetDictLists[i][2][0] - 50), int(newTargetDictLists[i][2][1] - 50)),
                           (int(newTargetDictLists[i][2][0]) + 100, int(newTargetDictLists[i][2][1]) + 100), (125, 0, 125), 4)
-            # print(i)
         # targetTrackTime 更新为Δt后：
-        newTargetDict["targetTrackTime"] = startTime + (deltaT)
+        newTargetDict["targetTrackTime"] = startTime + deltaT
         newTargetDict["nFrame"] = _nFrame
-        # [a, b] = newTargetDict["target"][0][2]
-        # cv2.rectangle(_frame, (int(a), int(b)), (int(a) + 50, int(b) + 50), (125, 0, 125), 4)
         # print("frameTime:" + str(newTargetDict["frameTime"]) + "     targetTrackTime:" + str(newTargetDict["targetTrackTime"])  + "     realTime:" + str(time.time()))
-        time.sleep(0.001)
-        print(newTargetDict, time.time())
+        time.sleep(0.009)
+        newTargetDict["timeCost"] = time.time()
+        print(newTargetDict)
 
         return newTargetDict
 
@@ -162,6 +161,23 @@ class Track:
                 # print(targetList)
         file.close()
 
+    def speedEstimate(self, targetDict_1, targetDict_2):
+        """
+
+        :param targetDict_1:
+        :param targetDict_2:
+        :return:
+        """
+        targetDictList_1 = targetDict_1.get("target")
+        targetDictList_2 = targetDict_2.get("target")
+        tempDeltaT = targetDict_2.get("timeCost") - targetDict_1.get("timeCost")
+        for i in range(len(targetDictList_1)):
+            targetDictList_2[i][3][0] = (targetDictList_2[i][2][0] - targetDictList_1[i][2][0]) / tempDeltaT
+            targetDictList_2[i][3][1] = (targetDictList_2[i][2][1] - targetDictList_1[i][2][1]) / tempDeltaT
+        print(targetDict_2)
+
+        return None
+
 
 if __name__ == "__main__":
     # cam, _image = imageInit()
@@ -213,7 +229,9 @@ if __name__ == "__main__":
 
                 if i == 9:
                     frame, nFrame, t = cam.getImage()
-                    tempDict = targetTracking.updateTarget(tempDict, time.time(), nFrame, flag, frame)  # 更新时，要求在targetTrackTime上做自加，这里updateTarget()还需改进！！
+                    print(nFrame)
+                    # 更新时，要求在targetTrackTime上做自加，在最后一次子循环 作判断
+                    tempDict = targetTracking.updateTarget(tempDict, time.time(), nFrame, flag, frame)
 
         else:
             tempDict = dict()
@@ -265,3 +283,13 @@ if __name__ == "__main__":
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
     cam.destroy()
+
+    # temp1 = {'target': [['5d5be4e8-3776-11ea-b96a-645d86dd3ea1', 0, [523.8499999999926, 517.8499999999995], [5, 5], 0, 0, 0]],
+    #  'nFrame': 0, 'bgTimeCost': 0.027158899999999875, 'timeCost': 1579079215.4694853,
+    #  'targetTrackTime': 1579079215.469035, 'frameTime': 1579079208.4893794}
+    # temp2 = {'target': [['5d5be4e8-3776-11ea-b96a-645d86dd3ea1', 0, [523.8999999999926, 517.8999999999994], [5, 5], 0, 0, 0]],
+    #  'nFrame': 0, 'bgTimeCost': 0.027158899999999875, 'timeCost': 1579079215.4792843,
+    #  'targetTrackTime': 1579079215.4795918, 'frameTime': 1579079208.4893794}
+    #
+    # track = Track()
+    # track.speedEstimate(temp1, temp2)
