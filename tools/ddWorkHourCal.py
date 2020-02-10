31a8696d29dea0a3a2b2ba97bc9cd86dd5df8a9e
xiaobiky123 <hujie119121@163.com>
2020-02-10 17:42:20 +0800

    modify the comment

--- tools/ddWorkHourCal.py
+++ tools/ddWorkHourCal.py
@@ -35,7 +35,9 @@ def capture(left, top, right, bottom):
 # 设定保存标定图片的路径calibDir，设定保存工作时间表的路径excelDir，
 # 然后运行程序，在标定图片路径下获得了标定图片，
 # 用画图软件打开这张图片，看左上角点的坐标，填写在left,top上，看一个格子的宽度和高度，填写在
-# gw，gh上,关闭程序
+# gw，gh上,根据钉钉视频上九宫格每个人的位置，修改sudoku字典人名的位置，每个人途中不得离开，
+# 可以关闭摄像头，否则位置会变化
+# 关闭程序
 # 再重新运行程序，投掷骰子，如果骰子掷为6，则随机时间保存屏幕截图，否则不保存，观察运行窗口，
 # 实时查看每个人的工作时间显示在左上角，小数位数较多，每分钟会更新一次
 # 工作结束按q退出,会保存每个人的工作时间在excel表中，excel表的文件名已经加上时间戳
@@ -61,27 +63,28 @@ top = 0
 gw = 637
 gh = 360
 
+work_hours = 0
+#九宫格的位置，每次需要根据实际的位置修改下列人名的位置
+sudoku = {
+          "Hujie":      [left,       left+gw,    top,       top+gh,    work_hours],
+          "LuChenYin":  [left+gw,    left+2*gw,  top,       top+gh,    work_hours],
+          "TaoTao":     [left+2*gw,  left+3*gw,  top,       top+gh,    work_hours],
+
+          "John":       [left,       left+gw,    top+gh,    top+2*gh,  work_hours],
+          "DaPeng":     [left+gw,    left+2*gw,  top+gh,    top+2*gh,  work_hours],
+          "Tina":       [left+2*gw,  left+3*gw,  top+gh,    top+2*gh,  work_hours],
+
+          "LouQiGe":    [left,       left+gw,    top+2*gh,  top+3*gh,  work_hours],
+          "FeiFei":    [left+gw,    left+2*gw,  top+2*gh,  top+3*gh,  work_hours],
+          "ZhiMing":     [left+2*gw,  left+3*gw,  top+2*gh,  top+3*gh,  work_hours]
+          }
 if __name__ == '__main__':
     # 调试参数  1分钟多少秒 正常运行情况下60  调试情况下可改小
     seconds = 60
     # 每个员工屏幕格子是否改变的阈值 以比值来写 适用于不同电脑的显示屏
     working_threshold = 500.0/(gw*gh)
     #初始化工作时间
-    work_hours = 0
-    #九宫格的位置，每次需要根据实际的位置修改下列人名的位置
-    sudoku = {
-              "Hujie":      [left,       left+gw,    top,       top+gh,    work_hours],
-              "LuChenYin":  [left+gw,    left+2*gw,  top,       top+gh,    work_hours],
-              "TaoTao":     [left+2*gw,  left+3*gw,  top,       top+gh,    work_hours],
 
-              "John":       [left,       left+gw,    top+gh,    top+2*gh,  work_hours],
-              "DaPeng":     [left+gw,    left+2*gw,  top+gh,    top+2*gh,  work_hours],
-              "Tina":       [left+2*gw,  left+3*gw,  top+gh,    top+2*gh,  work_hours],
-
-              "LouQiGe":    [left,       left+gw,    top+2*gh,  top+3*gh,  work_hours],
-              "FeiFei":    [left+gw,    left+2*gw,  top+2*gh,  top+3*gh,  work_hours],
-              "ZhiMing":     [left+2*gw,  left+3*gw,  top+2*gh,  top+3*gh,  work_hours]
-              }
 
     # 这里要按照上面sudoku的顺序填写
     title = []
