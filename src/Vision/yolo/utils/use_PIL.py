232e929424a9523e6b23cdcdabfe72219227b737
Dony <yuantao@126.com>
2020-02-12 17:05:12 +0800

    完善自动生成数据集功能。利用自动生成的1080张图，加上之前生成的540张，共1620张图，进行训练

--- src/Vision/yolo/utils/use_PIL.py
+++ src/Vision/yolo/utils/use_PIL.py
@@ -1,4 +1,5 @@
 import os
+import random
 from datetime import datetime
 
 from PIL import Image, ImageDraw
@@ -9,16 +10,16 @@ class Aug(object):
     """利用图片贴合的方式生成数据集"""
     def __init__(self, src, bg, btype):
         # self.init_bg = bg
-        self.src = src
-        self.bg = bg
-        self.cp_bg = None
-        self.box = None
-        self.bgbox = None
-        self.rotated_img = None
-        self.type = btype
-        self.n = 0
-        self.file_name = None
-        self.center = (100, 400)
+        self.src = src  # <Image>:需要贴合的bottle
+        self.bg = bg  # <Image>:背景
+        self.cp_bg = None  # <Image>:拷贝一份背景用来处理，防止对原始背景图片的污染
+        self.box = None  # tuple:src图片中物体的位置(xmin, ymin, xmax, ymax)
+        self.bgbox = None  # tuple:贴合后，bottle位于背景中的位置(xmin, ymin, xmax, ymax)
+        self.rotated_img = None  # <Image>:转换后的图片
+        self.type = btype  # int/str:标注时的瓶子类别(0-21)
+        self.n = 0  # int:用于防止图片名称重复
+        self.file_name = None  # str:文件不带后缀的名称，如_img000.jpg,则file_name=_img000
+        self.center = None  # tuple:eg->(200, 400)
 
     def rotate_src(self, angle):
         """
@@ -80,20 +81,34 @@ class Aug(object):
         draw = ImageDraw.Draw(self.cp_bg)
         # draw.line()
         draw.rectangle(self.bgbox, outline="red", width=2)
-        # 画完框之后，显示
-        self.cp_bg.show()
+        # 画完框之后，显示，注意：如果开启显示，执行速度降低而且要保证你的内存足够大。生产使用，建议关闭
+        # self.cp_bg.show()
+
         # self.bg = self.init_bg
         # self.bg.show()
         del draw
 
     def img_txt(self):
-        self.bgbox = (self.center[0] + self.box[0],
+        self.bgbox = [self.center[0] + self.box[0],
                       self.center[1] + self.box[1],
                       self.center[0] + self.box[2],
-                      self.center[1] + self.box[3])
+                      self.center[1] + self.box[3]]
+        # 处理物体超出边界的情况
+        self.bgbox[0] = self.bgbox[0] if 0 <= self.bgbox[0] else 0
+        self.bgbox[1] = self.bgbox[1] if 0 <= self.bgbox[1] else 0
+        self.bgbox[2] = self.bgbox[2] if self.bgbox[2] <= self.bg.size[0] else self.bg.size[0]
+        self.bgbox[3] = self.bgbox[3] if self.bgbox[3] <= self.bg.size[1] else self.bg.size[1]
         with open("anno/" + self.file_name + ".txt", "w") as f:
             f.write(str(int(self.bgbox[0])) + "," + str(int(self.bgbox[1])) + "," +
                     str(int(self.bgbox[2])) + "," + str(int(self.bgbox[3])) + "," + str(self.type))
+
+    def random_center(self):
+        # 随机生成贴合中心点
+        x_random = random.randint(50, 1100)
+        # x_random = random.randint(1150, 1200)
+        y_random = random.randint(350, 550)
+        # y_random = random.randint(800, 900)
+        self.center = (x_random, y_random)
 
     def stack_up(self):
         """
@@ -105,10 +120,13 @@ class Aug(object):
         """
         # dst.paste(src_img, (100, 400), src_img)
         self.cp_bg = self.bg.copy()
+        # 随机生成贴合中心点坐标
+        self.random_center()
         self.cp_bg.paste(self.rotated_img, self.center, self.rotated_img)
         # 生成文件名，尽量保证唯一
         t = datetime.strftime(datetime.today(), "%Y%m%d%H%M%S")
         self.cp_bg.save("dataset/" + t + str(self.n) + ".jpg")
+        print("已保存类别{}，{}张!".format(self.type, self.n+1))
         self.file_name = t + str(self.n)
         # todo 生成txt文件
         self.img_txt()
@@ -117,8 +135,9 @@ class Aug(object):
 
 def main():
     aug = Aug(src, bg, btype)
-    for i in range(5):
-        angle = (i + 1) * 10
+    for angle in range(360):
+        if not angle % 2 == 0:
+            continue
         aug.rotate_src(angle)
         aug.get_obj_box()
         aug.stack_up()
@@ -148,15 +167,19 @@ def get_xy():
 
 
 if __name__ == '__main__':
-    btype = 1
+    src_path = "images/bottle_02_03.png"
+    bg_path = "images/bg.jpg"
+    # 根据文件名进行类别提取。文件名如：bottle_(type)_(number).png
+    btype = int(src_path.split("_")[1])
+
     # 生成的图片统一保存到dataset文件夹
     if not os.path.exists("dataset"):
         os.mkdir("dataset")
     if not os.path.exists("anno"):
         os.mkdir("anno")
     # bottle need to paste
-    src = Image.open("images/bottle_01.png").convert("RGBA")
+    src = Image.open(src_path).convert("RGBA")
     # background
-    bg = Image.open("images/bg.jpg")
+    bg = Image.open(bg_path)
     main()
 
