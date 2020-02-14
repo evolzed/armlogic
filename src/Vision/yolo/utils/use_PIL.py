88015c563a10820893614599cc7607681c2dfdb5
Dony <yuantao@126.com>
2020-02-14 23:23:55 +0800

    采用imgAug第三方库，对多张背景进行随机增强处理。然后再贴合。这样更符合实际场景：TODO一个待修复的小bug还未解决[2020-02-14-23:23]。

--- src/Vision/yolo/utils/use_PIL.py
+++ src/Vision/yolo/utils/use_PIL.py
@@ -4,6 +4,8 @@ from datetime import datetime
 
 from PIL import Image, ImageDraw, ImageEnhance
 import numpy as np
+import cv2
+import imgaug.augmenters as iaa
 
 n = 0  # 记录图片的数量
 
@@ -14,6 +16,8 @@ class Aug(object):
         # self.init_bg = bg
         self.src = src  # <Image>:需要贴合的bottle
         self.bg = bg  # <Image>:背景
+        self.bg_prod = None  # 增强过后的背景
+        self.bg_arr = np.array(self.bg)  # 将背景图转化为[cv2格式, 未涉及通道变换，可选转通道]的ndarray
         self.cp_bg = None  # <Image>:拷贝一份背景用来处理，防止对原始背景图片的污染
         # self.cp_bg_gama = None  # <Image>:拷贝一份背景用来处理，防止对原始背景图片的污染
         self.box = None  # tuple:src图片中物体的位置(xmin, ymin, xmax, ymax)
@@ -25,6 +29,23 @@ class Aug(object):
         self.center = None  # tuple:eg->(200, 400)
         self.img_path = img_path
         self.anno_path = anno_path
+        self.seq = iaa.OneOf([
+            iaa.Crop(px=(0, 100)),  # 随机从图像的每个边裁剪掉0~100个像素值，保持图像原尺寸不变
+            iaa.Fliplr(0.4),  # 有0.5的概率对图像进行左右翻转
+            iaa.Flipud(0.4),  # 有0.5的概率对图像进行上下翻转
+            iaa.GaussianBlur(sigma=(0, 3.0)),  # 进行高斯模糊，范围0【不变】~3.0【最大】
+            iaa.Affine(
+                scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},  # x，y缩放比例
+                translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},  # x,y方向上的偏移范围是-0.2~0.2
+                rotate=(0, 360),  # 随机旋转范围
+            )
+        ])
+
+    def img_augm(self):
+        """对背景进行随机增强处理"""
+        self.bg_arr = self.seq.augment_image(self.bg_arr)
+        # 增强后的bg_prod用于背景贴合
+        self.bg_prod = Image.fromarray(self.bg_arr)
 
     def rotate_src(self, angle):
         """
@@ -105,10 +126,10 @@ class Aug(object):
                     str(int(self.bgbox[2])) + "," + str(int(self.bgbox[3])) + "," + str(self.type))
 
     def random_center(self):
-        # 随机生成贴合中心点
-        x_random = random.randint(50, 1100)
+        # 随机生成贴合坐标，left-up
+        x_random = random.randint(0, 1100)
         # x_random = random.randint(1150, 1200)
-        y_random = random.randint(350, 550)
+        y_random = random.randint(0, 800)
         # y_random = random.randint(800, 900)
         self.center = (x_random, y_random)
 
@@ -142,11 +163,13 @@ class Aug(object):
 
         :return:
         """
-        self.cp_bg = self.bg.copy()
+        self.img_augm()
+        self.cp_bg = self.bg_prod
+        # self.cp_bg = self.bg_arr
         # 对bg进行处理，随机生成0.6-1.5之间的factor，调整bg的亮度
         self.cp_bg = ImageEnhance.Brightness(self.cp_bg).enhance(round(random.uniform(0.6, 1.5), 1))
         # 对瓶子进行处理
-        self.rotated_img = ImageEnhance.Brightness(self.rotated_img).enhance(round(random.uniform(0.6, 1.5), 1))
+        self.rotated_img = ImageEnhance.Brightness(self.rotated_img).enhance(round(random.uniform(0.5, 1.2), 1))
 
 
 def main(img_num):
@@ -172,52 +195,35 @@ def main(img_num):
         # aug.draw_box()
 
 
-def get_xy():
-    """
-    用于测试rgba模式
-
-    :return: None
-    """
-    dst = Image.open("images/bg.jpg").convert("RGBA")
-    # 旋转目标图片
-    dst = dst.rotate(45, expand=True)
-    w1, h2 = dst.size
-    color_0 = (0, 0, 0)
-    for m in range(w1):
-        for n in range(h2):
-            dot = (m, n)
-            color_1 = dst.getpixel(dot)
-            if color_1 == color_0:
-                color_1 = color_1[:-1] + (0,)
-                dst.putpixel(dot, color_1)
-    # 遍历结束，已经修改dst完成，返回
-    return dst
-
-
 if __name__ == '__main__':
     img_save_path = "dataset_13"
     anno_save_path = "anno_13"
-    img_total_num = 1000
+    img_total_num = 200
     # 生成的图片统一保存到dataset文件夹
     if not os.path.exists(img_save_path):
         os.mkdir(img_save_path)
     if not os.path.exists(anno_save_path):
         os.mkdir(anno_save_path)
     # 将需要的物体存放列表
-    bottle_list = ["01", "02", "03", "04", "05"]
+    # bottle_list = ["01", "02", "03", "04", "05", "06"]
+    bottle_list = ["%02d" % (i + 1) for i in range(6)]  # 选取6张bottle透明背景图
+    # bg_id = ["01", "02", "03"]
+    bg_id = ["%02d" % (i + 1) for i in range(10)]  # 选取10张背景图
     bottle_num = len(bottle_list)
     for i in range(bottle_num):
         # bottle need to paste
         src_path = "images/bottle_01_{}.png".format(bottle_list[i])
         # background
-        bg_path = "images/bg.jpg"
+        bg_path = "images/bg_{}.jpg".format(random.sample(bg_id, 1)[0])
         btype = int(src_path.split("_")[1])
         src = Image.open(src_path).convert("RGBA")
-        # background
+        # background, 利用PIL读取背景
         bg = Image.open(bg_path)
+        # # 使用img_aug库对背景进行处理，但需要先将图片转化成ndarray数组
+        # bg = cv2.imread(bg_path)
         # 根据总数量，计算每个图片需要的个数。保证数据均衡
         img_num = img_total_num // bottle_num
-        # 如果不能整除，这一步处理，统一添加到最后一张图上
+        # 如果不能整除，这一步处理，将多余的数据统一添加到最后一张图上进行处理
         if i == bottle_num - 1:
             img_num = img_num + img_total_num % bottle_num
         main(img_num)
