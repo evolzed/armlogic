0b1e6602386e978df0b428206e10566269eb30fc
ge <louqgmr@163.com>
2020-03-09 15:53:04 +0800

    继续完善数据处理功能，并添加相关注释

--- tools/DataProcess.py
+++ tools/DataProcess.py
@@ -20,12 +20,13 @@ class DataProcess():
         """
             初始化信息：姓名、数据源路径、输出路径、处理时间
         """
-        # self.name = input("input name")
-        # self.inPath = input("input data path")
-        # while not os.path.isdir(self.inPath):
-        #     self.inPath=input("Reenter data path")
-        '''测试用'''
-        self.inPath = r'F:\XSCX\Samples\validation\dtprcs'
+        self.name = input("input name：")
+        self.inPath = input("input data path")
+        while not os.path.isdir(self.inPath):
+            self.inPath=input("Reenter data path")
+
+        # self.inPath = r'F:\XSCX\Samples\validation\dtprcs'#测试用
+        
         self.outPath=os.path.join(self.inPath, 'outputs')
         self.dataTime = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
         self.yolo = YOLO()
@@ -66,6 +67,8 @@ class DataProcess():
 
             self.SaveAnnotation2JSON(newName, result['box'], size)  # 保存标注信息
             id += 1
+        with open(os.path.join(self.outPath,'annotation.txt'),'w') as fd:
+            fd.write('标注范围：%s-%s\n标注数量：%s\n标注时间：%s\n标注人员：%s\n'%(str(self.dataTime)+'0',str(self.dataTime)+str(id-1),id,self.dataTime,self.name))
 
     def SaveAnnotation2JSON(self, fname, outputs, size):
         '''
@@ -89,8 +92,9 @@ class DataProcess():
         ann["size"] = size
         ann["path"] = '%s.jpg' % fname
 
-        # 保存数据到文件
-        fp = os.path.join(self.outPath, '%s.json' % fname)  # 文件保存路径
+        # 保存数据到文件↓
+        # 文件保存路径
+        fp = os.path.join(self.outPath, '%s.json' % fname)
         if not os.path.exists(self.outPath):
             os.mkdir(self.outPath)
         try:
@@ -101,13 +105,25 @@ class DataProcess():
         return 0
 
     def FilterAnnotation(self):
+        '''
+        对标注信息进行过滤，记录不符合要求的图片
+        :return: None
+        '''
+        # 获取文件列表
         fileList = os.listdir(self.outPath)
+        # 统计未标注的文件
         un_anno = []
         for file in fileList:
+            #遍历文件列表
+            #判断文件类型
             if file.endswith(".json"):
+                #打开json文件
                 with open(os.path.join(self.outPath,file), "r") as f:
+                    #解析json文件
                     dic = json.loads(f.read().encode("gbk").decode("utf8"))
+                    #获取目标列表
                     outputs = dic.get("outputs")
+                    #判断是否有目标
                     if outputs is None:
                         print("--None--" * 20, file)
                         continue
@@ -116,9 +132,13 @@ class DataProcess():
                     except Exception as e:
                         un_anno.append(file)
                         continue
+                    #不包含后缀的文件名
                     name=file[:-5]
+                    #打开对应的图像文件
                     with pimg.open(os.path.join(self.inPath,'%s.jpg'% name)) as img:
+                        #创建画图对象
                         imgDraw=draw.Draw(img)
+                        #获取所有的目标框
                         for bbox in obj:
                             bbox_class = bbox["name"]
                             bbox_xmin = bbox["bndbox"]["xmin"]
@@ -126,17 +146,18 @@ class DataProcess():
                             bbox_xmax = bbox["bndbox"]["xmax"]
                             bbox_ymax = bbox["bndbox"]["ymax"]
                             imgDraw.rectangle((bbox_xmin,bbox_ymin,bbox_xmax,bbox_ymax))
-                            # print(bbox_class,bbox_xmin,bbox_ymin,bbox_xmax,bbox_ymax)
+                        #显示图像
                         img.show()
-                        flag=input('是否需要重标,请输入 y 或 n')
+                        #标记数据是否需要重新标注
+                        flag=input('是否需要重标,请输入 y 或 n：')
                         while not flag in ['y','n']:
-                            flag = input('输入错误,请重新输入 y 或 n')
+                            flag = input('输入错误,请重新输入 y 或 n：')
                         if flag=='n':
                             continue
                         elif flag=='y':
+                            #需要重新标注时，将文件名写入txt文档
                             with open(os.path.join(self.outPath, "reList.txt"),'a') as fd:
                                 fd.write('%s.jpg\n'% name)
-                                print(name)
 
 
 if __name__ == '__main__':
